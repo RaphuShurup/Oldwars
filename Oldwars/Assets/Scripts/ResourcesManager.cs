08644d013a059bc83fcce5e03d625ebce2970886
Emin06 <Emincan.dogan@hotmail.com>
2022-07-13 17:48:30 +0300

    a

--- Oldwars/Assets/Scripts/ResourcesManager.cs
+++ Oldwars/Assets/Scripts/ResourcesManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourcesManager : MonoBehaviour
 {
     public static ResourcesManager Instance;
+
+    public Text woodcounttext;
+    public Text stonecounttext;
 
     private void Awake()
     {
@@ -17,11 +21,13 @@ public class ResourcesManager : MonoBehaviour
     public void SetWoodData(int value)
     {
         scriptable.wood += value;
+        woodcounttext.text = (scriptable.wood).ToString();
     }
 
     public void SetStoneData(int value)
     {
         scriptable.stone += value;
+        stonecounttext.text = (scriptable.stone).ToString();
     }
 
     public int GetWoodData()
