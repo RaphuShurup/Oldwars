4b2f3990fda15e49c382e836f4413f8d9db87169
Emin06 <Emincan.dogan@hotmail.com>
2022-07-13 17:34:53 +0300

    Toplama sistemleri düzeltme

--- Oldwars/Assets/Scripts/TrigerController.cs
+++ Oldwars/Assets/Scripts/TrigerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class TrigerController : MonoBehaviour
 {
     private Rigidbody rb;
+    public bool controltime;
+    public Animator animcontrol;
 
     private void Start()
     {
+        animcontrol = gameObject.GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -16,25 +19,59 @@ public class TrigerController : MonoBehaviour
         rb.velocity = Vector3.zero;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    public void Test()
+    {
+        controltime = true;
+        Debug.Log("a");
+    }
+
+    private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Stone"))
         {
-            ResourcesManager.Instance.SetStoneData(5);
-            CloseVelocity();
+            animcontrol.SetBool("Stone", true);
+            if (controltime == true)
+            {
+                ResourcesManager.Instance.SetStoneData(5);
+                CloseVelocity();
+                controltime = false;
+            }
+
         }
         else if (collision.gameObject.CompareTag("Tree"))
         {
-            ResourcesManager.Instance.SetWoodData(5);
-            CloseVelocity();
+            animcontrol.SetBool("Tree", true);
+            if (controltime == true)
+            {
+                ResourcesManager.Instance.SetWoodData(5);
+                CloseVelocity();
+                controltime = false;
+            }
+
+
         }
         else if (collision.gameObject.CompareTag("WoodBuild"))
         {
-           //UI dan paneli aç. Onaylarsa bizden kaynaðý eksilt ve binayý yap.
+            //UI dan paneli aç. Onaylarsa bizden kaynaðý eksilt ve binayý yap.
         }
         else if (collision.gameObject.CompareTag("StoneBuild"))
         {
             //UI dan paneli aç. Onaylarsa bizden kaynaðý eksilt ve binayý yap
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Stone"))
+        {
+            animcontrol.SetBool("Stone", false);
+            controltime = false;
+        }
+
+        if (collision.gameObject.CompareTag("Tree"))
+        {
+            animcontrol.SetBool("Tree", false);
+            controltime = false;
         }
     }
 
